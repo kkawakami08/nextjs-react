db198fe0dd7af1eac699b9cb210d4e1e6de0bda1
Kaori Kawakami <k.kawakami08@gmail.com>
2023-03-19 15:25:41 -0700

    footer styling and links

--- nextjs-react/components/Footer.tsx
+++ nextjs-react/components/Footer.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import githubIcon from '../public/github-icon-white.png'
+import emailIcon from '../public/email-icon-white.png'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <div className="bg-emerald-900">Footer</div>
+    <div className="bg-emerald-900 flex flex-col items-center p-5">
+        <p className='opacity-80 text-sm text-emerald-50 font-thin'>Copyright © 2023 Kaori Kawakami</p>
+        <ul className='flex gap-10'>
+            <li>
+                <a target="_blank" href="https://mail.google.com/mail/?view=cm&fs=1&to=k.kawakami08@gmail.com" className='opacity-80 text-sm text-emerald-50 font-thin underline' >
+                    Email
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://github.com/kkawakami08" className='opacity-80 text-sm text-emerald-50 font-thin underline' >
+                Github
+                </a>
+            </li>
+        </ul>
+        
+    </div>
   )
 }
