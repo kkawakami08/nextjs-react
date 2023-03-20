087a0aedc07cf34356f1384a19a46a6b0f02ec9c
Kaori Kawakami <k.kawakami08@gmail.com>
2023-03-20 09:43:37 -0700

    add my hero image

--- nextjs-react/components/Header.tsx
+++ nextjs-react/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import kaoriImg from '../public/kaori-hero-square.jpg'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='w-56 h-56 bg-black overflow-hidden rounded-full relative mx-auto'>
+      <Image 
+        src={kaoriImg}
+        alt="Kaori"
+        width={310}
+        height={310}
+        />
+      </div>
   )
 }
